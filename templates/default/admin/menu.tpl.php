83ccf54a0f95590809e1903b46ac43fa77a86bd6
Marcus Povey <marcus@marcus-povey.co.uk>
2013-07-04 16:15:43 +0100

    Restoring path changes

--- templates/default/admin/menu.tpl.php
+++ templates/default/admin/menu.tpl.php
@@ -1,9 +1,9 @@
 <div class="navbar">
     <div class="navbar-inner">
         <ul class="nav">
-            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/') echo 'class="active"'; ?>><a href="/admin/" >Administration</a></li>
-            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/plugins/') echo 'class="active"'; ?>><a href="/admin/plugins/">Plugins</a></li>
-            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/dependencies/') echo 'class="active"'; ?>><a href="/admin/dependencies/">Dependencies</a></li>
+            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/') echo 'class="active"'; ?>><a href="<?= \Idno\Core\site()->config()->url; ?>admin/" >Administration</a></li>
+            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/plugins/') echo 'class="active"'; ?>><a href="<?= \Idno\Core\site()->config()->url; ?>admin/plugins/">Plugins</a></li>
+            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/dependencies/') echo 'class="active"'; ?>><a href="<?= \Idno\Core\site()->config()->url; ?>admin/dependencies/">Dependencies</a></li>
             <?=$this->draw('admin/menu/items')?>
         </ul>
     </div>
